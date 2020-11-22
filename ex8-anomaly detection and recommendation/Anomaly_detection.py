29ed31d5078381e182b5e9b1a12e70ced2a644b5
chenshaobin <13531194616@163.com>
2020-11-22 22:39:55 +0800

    update

---
+++ ex8-anomaly detection and recommendation/Anomaly_detection.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(context="notebook", style="white", palette=sns.color_palette("RdBu"))
+
+import numpy as np
+import pandas as pd
+import scipy.io as sio
+from scipy import stats
+from sklearn.model_selection import train_test_split
+
